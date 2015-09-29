ed8fb265f665d2440562797daedb3ff99343758e
Marek Osvald <drekorian@drekorian.cz>
2015-09-29 23:42:28 +0200

    Adds TheMovieDB API key

---
+++ app/src/main/java/cz/muni/fi/xosvald/pv256/TheMovieDB.java
@@ -0,0 +1,14 @@
+package cz.muni.fi.xosvald.pv256;
+
+/**
+ * Static class that holds constants.
+ *
+ * Created by Marek Osvald on 29.09.2015.
+ */
+public class TheMovieDB {
+    public static final String API_KEY = "9b423b1fe1dca6655d13348684dce5d4";
+
+    private TheMovieDB() {
+    }
+
+}
